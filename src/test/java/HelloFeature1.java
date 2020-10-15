fccad315330a21ebe5e4048477e6282e7eaeee23
EU3 Batch <yavuzyesilyurt3@gmail.com>
2020-10-15 22:33:26 +0100

    added some text

--- src/test/java/HelloFeature1.java
+++ src/test/java/HelloFeature1.java
@@ -1,5 +1,7 @@
 public class HelloFeature1 {
     public static void main(String[] args) {
         System.out.println("hello feature1");
+
+        System.out.println("ı am fine , what about you");
     }
 }
