473156c10cc23e72e427cc6fcc5e3c8fc408fa68
YAVUZ YESİLYURT <65642458+yvzyslyrt@users.noreply.github.com>
2020-10-16 10:16:51 +0100

    Update Hello.java

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,9 +2,11 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("hello git");
+        System.out.println("hello FROM THE GITHUB TO  git");
 
         System.out.println("are you ok?");
+
+
 
     }
 }
